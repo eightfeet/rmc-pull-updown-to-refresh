6151ae805fc9a71d52ab820b70d1d91426108c9e
eightfeet <bajiao000000@163.com>
2018-07-11 11:30:40 +0800

    remove test

--- examples/src/components/PullToRefresh/__tests__/index-test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env jest */
-import PullToRefresh from './../../../index';
-import PullToRefreshComponent from './../PullToRefresh';
-
-describe('index.js', () => {
-  it('should correctly export component as named export', () => {
-    expect(PullToRefresh).toEqual(PullToRefreshComponent);
-  });
-});
